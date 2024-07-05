4b14032b9914f7ca746289a5a49f2a50cbe75e41
miRRor1309 <145328904+miRRor1309@users.noreply.github.com>
2024-07-05 13:31:38 +0700

    Add project files.

---
+++ Controllers/UserInfoesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoAn.Models;
+
+namespace DoAn.Controllers
+{
+    public class UserInfoesController : Controller
+    {
+        private DoAnEntities db = new DoAnEntities();
+
+        // GET: UserInfoes
+        public ActionResult Index()
+        {
+            return View(db.UserInfoes.ToList());
+        }
+
+        // GET: UserInfoes/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserInfoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "maUser,SDT,AdminOrCus,TenTK,PassWord,Email")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserInfoes.Add(userInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "maUser,SDT,AdminOrCus,TenTK,PassWord,Email")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            db.UserInfoes.Remove(userInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult LogResView()
+        {
+            return View();
+        }
+    }
+}
